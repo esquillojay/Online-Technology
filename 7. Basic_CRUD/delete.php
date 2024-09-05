7d15a3f2ecbddca8223e0c602735d68fea597f48
yaj <159892252+esquillojay@users.noreply.github.com>
2024-09-05 18:55:49 +0800

    Add files via upload

---
+++ 7. Basic_CRUD/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+	require('connection.php');
+
+	$id = $_GET['id'];
+
+	$sql_cmd = "DELETE FROM tblstudents WHERE studentid = :id";
+
+	$stmt = $conn->prepare($sql_cmd);
+
+	$stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+	$stmt->execute();
+
+	header("Location: index.php");
+
+
+?>
